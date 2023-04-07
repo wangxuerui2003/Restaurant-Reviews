e27445bd8712ee4561ba7e9f441dfd628c987328
Wang Xuerui <wangxuerui2003@gmail.com>
2023-04-07 17:46:51 +0800

    Some comments on CORS

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express"
-import cors from "cors"
+import cors from "cors" // Cross-Origin Resources Sharing, in my opinion some kind of security for send/receive data between different origins (domains)
 import restaurants from "./api/restaurants.route.js"
 
 const app = express();
