3576c8aad1c3c614998db7486830394dec47187f
Wang Xuerui <wangxuerui2003@gmail.com>
2023-04-07 20:38:11 +0800

    Add routes for post, put, delete reviews, get list of cuisines and get restaurant by id

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,5 +1,6 @@
 import express from "express"
 import RestaurantsCtrl from "./restaurants.controller.js"
+import ReviewsCtrl from "./reviews.controller.js"
 
 // Routing in express
 const router = express.Router();
@@ -9,6 +10,20 @@ const router = express.Router();
 // 	res.send("Hello World!");
 // });
 
+// Get list of restaurants
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
+
+// Get list of cuisines
+router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines);
+
+// Get restaurant by id
+router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById);
+
+
+// POST, PUT, DELETE reviews
+router.route("/review")
+	.post(ReviewsCtrl.apiPostReview)
+	.put(ReviewsCtrl.apiUpdateReview)
+	.delete(ReviewsCtrl.apiDeleteReview);
 
 export default router;
