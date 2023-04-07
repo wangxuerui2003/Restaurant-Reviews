5111451b0fbad8a60e6bb4c4f2f521655075a375
Wang Xuerui <wangxuerui2003@gmail.com>
2023-04-07 17:46:16 +0800

    Add the default API route

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,11 +1,14 @@
 import express from "express"
+import RestaurantsCtrl from "./restaurants.controller.js"
 
 // Routing in express
 const router = express.Router();
 
 // Demo route
-router.route("/").get((req, res) => {
-	res.send("Hello World!");
-});
+// router.route("/").get((req, res) => {
+// 	res.send("Hello World!");
+// });
+
+router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
 
 export default router;
