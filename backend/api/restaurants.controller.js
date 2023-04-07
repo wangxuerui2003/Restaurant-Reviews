90ed1037c2ee505332f2e17d5f77949e4913c05c
Wang Xuerui <wangxuerui2003@gmail.com>
2023-04-07 18:15:07 +0800

    create the RestaurantsController class and add docs

---
+++ backend/api/restaurants.controller.js
@@ -0,0 +1,43 @@
+import RestaurantsDAO from "../dao/restaurantsDAO.js"
+
+export default class RestaurantsController {
+	/**
+	 * Parse the API request and init the filters, page and restaurantsPerPage variables.
+	 * Then pass them into the getRestaurants method and assign the response to 
+	 * restaurantsList and totalNumRestaurants.
+	 * Use the res function passed in by Express and send json data as response.
+	 * 
+	 * @param {*} req 
+	 * @param {*} res 
+	 * @param {*} next 
+	 */
+	static async apiGetRestaurants(req, res, next) {
+		const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.query.restaurantsPerPage, 10) : 20;
+		const page = req.query.page ? parseInt(req.query.page, 10) : 0
+
+		let filters = {};
+		if (req.query.cuisine) {
+			filters.cuisine = req.query.cuisine;
+		} else if (req.query.zipcode) {
+			filters.zipcode = req.query.zipcode;
+		} else if (req.query.name) {
+			filters.name = req.query.name;
+		}
+
+		const { restaurantsList, totalNumRestaurants } = await RestaurantsDAO.getRestaurants({
+			filters,
+			page,
+			restaurantsPerPage
+		});
+
+		// The json data that is going to be responsed.
+		let response = {
+			restaurants: restaurantsList, // The restaurants
+			page: page,
+			filters: filters,
+			entries_per_page: restaurantsPerPage,
+			total_results: totalNumRestaurants
+		}
+		res.json(response);
+	}
+}
